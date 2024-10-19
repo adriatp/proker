a899c0ec43fa3d687e1b76c918e8f348988ed3d5
adriatp <tarradas.adria@gmail.com>
2024-10-19 18:27:13 +0200

    fix layout for cards modal

--- prokerts/example/js/script.js
+++ prokerts/example/js/script.js
@@ -1,23 +1,18 @@
 loadCards = () => {
   const modal_cards = document.querySelector('#modal_cards');
   modal_cards.textContent = '';
-  let positionX = 0, positionY = 0;
   for (let i=0; i<4; i++) {
-    positionX = 0;
     const row = document.createElement('div');
-    row.classList.add('row');
-    const col = document.createElement('div');
-    col.classList.add('col-12');
+    row.classList.add('row', 'justify-content-center');
     for (let j=0; j<13; j++) {
-      const card = document.createElement('div');
-      card.classList.add('card');
-      const url = `url('./assets/images/cards/${(j+1)+(i*13)}.png')`;
-      card.style.backgroundImage= `url('${url}')`;
-      col.appendChild(card);
-      positionX += -50;
+      const card_div = document.createElement('div');
+      const card_img = document.createElement('img');
+      card_div.classList.add('col-auto', 'p-1');
+      card_img.classList.add('img-fluid', 'card');
+      card_img.src = `./assets/images/cards/${(j+1)+(i*13)}.png`;
+      card_div.appendChild(card_img);
+      row.appendChild(card_div);
     }
-    positionY += -50;
-    row.appendChild(col);
     modal_cards.appendChild(row);
   }
 }
